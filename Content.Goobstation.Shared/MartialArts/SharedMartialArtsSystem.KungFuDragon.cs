e1f86b473391369223c942541cdd15460ba70b29
Ishkabelamuda <fish@zcx.pw>
2026-06-20 17:34:31 -0400

    Change HOS Belt to Grant Kung Fu Dragon instead of Capoeira (#1101)
    * make it so that Kung Fu Dragon martial art can be granted via clothing
    
    * make it so that the HOS belt grants Kung Fu Dragon instead of Capoeira
    
    * correct typo

--- Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.KungFuDragon.cs
+++ Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.KungFuDragon.cs
@@ -6,12 +6,17 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Common.MartialArts; // Omu
+using Content.Goobstation.Maths.FixedPoint; // Omu
 using Content.Goobstation.Shared.MartialArts.Components;
 using Content.Goobstation.Shared.MartialArts.Events;
 using Content.Shared._Shitmed.Targeting;
+using Content.Shared.Clothing; // Omu
+using Content.Shared.Damage; // Omu
 using Content.Shared.Interaction.Events;
 using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Stunnable;
+using Content.Shared.Weapons.Melee; // Omu
 using Content.Shared.Weapons.Melee.Events;
 using Robust.Shared.Prototypes;
 
@@ -27,8 +32,49 @@ public abstract partial class SharedMartialArtsSystem
 
         SubscribeLocalEvent<GrantKungFuDragonComponent, UseInHandEvent>(OnGrantCQCUse);
 
+        // Omu start
+        SubscribeLocalEvent<GrantKungFuDragonComponent, ClothingGotEquippedEvent>(OnWear);
+        SubscribeLocalEvent<GrantKungFuDragonComponent, ClothingGotUnequippedEvent>(OnRemove);
+        // Omu end
+
         SubscribeLocalEvent<DragonPowerBuffComponent, AttackedEvent>(OnAttacked);
     }
+
+    // Omu start
+    private void OnWear(EntityUid ent, GrantKungFuDragonComponent component, ref ClothingGotEquippedEvent args)
+    {
+        if (!_netManager.IsServer)
+            return;
+
+        var user = args.Wearer;
+        TryGrantMartialArt(user, component);
+    }
+
+    private void OnRemove(Entity<GrantKungFuDragonComponent> ent, ref ClothingGotUnequippedEvent args)
+    {
+        var user = args.Wearer;
+
+        // Omu Station
+        // Don't proceed if the user has non-removable Martial Arts knowledge
+        if (HasManualCqcKnowledge(user))
+            return;
+
+        if (!TryComp<MartialArtsKnowledgeComponent>(user, out var martialartsknowcomp)
+            || !TryComp<MeleeWeaponComponent>(user, out var meleeweaponcomp))
+            return;
+
+        if (martialartsknowcomp.MartialArtsForm != MartialArtsForms.KungFuDragon)
+            return;
+
+        var originalDamage = new DamageSpecifier();
+        originalDamage.DamageDict[martialartsknowcomp.OriginalFistDamageType]
+            = FixedPoint2.New(martialartsknowcomp.OriginalFistDamage);
+        meleeweaponcomp.Damage = originalDamage;
+
+        RemComp<MartialArtsKnowledgeComponent>(user);
+        RemComp<CanPerformComboComponent>(user);
+    }
+    // Omu end
 
     private void OnAttacked(Entity<DragonPowerBuffComponent> ent, ref AttackedEvent args)
     {
